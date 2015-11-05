9b8eabc7365276b91d328ac1407b302736091ab5
Sytnik13 <sytnik16@gmail.com>
2015-11-05 20:48:59 +0200

    Car DB

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function($, _){
+    'use strict';
+
+    var $minYearEl = $('#minYears'),
+        $maxYearEl = $('#maxYears');
+
+    $minYearEl.on('change', function(e) {
+        $maxYearEl.removeArr('disabled');
+        alert('asdasd');
+    });
+    
+})(jQuery, _);
